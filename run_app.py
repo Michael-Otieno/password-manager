8888cf8e5d83494d34d1f79fd2299e7b51129e4b
michael otieno <michael.otieno@student.moringaschool.com>
2021-09-05 19:53:51 +0300

    add changes

--- run_app.py
+++ run_app.py
@@ -99,7 +99,7 @@ def main():
         print('Invalid choice. Please Try again')
 
     while True:
-        print("Use the words : ADD - add a new account, DI - display all accounts, find - find an account, exit - exit the program")
+        print("Use the words : ADD - add a new account, DI - display all accounts, FI - find an account, E - exit the program")
         short_code = input().lower()
         if short_code == "add":
             print("Add account")
@@ -137,232 +137,26 @@ def main():
             else:
                 print("You don't seem to have an account use AD to add account")
 
+        elif short_code == "fi":
+            print('\n')
+            print("Enter the account name: ")
+            account_name = input()
+            if find_credential(account_name):
+                print("-"*10)
+                print(f"Account: {find_credential.account}")
+                print(f"Username: {find_credential.username}")
+                print(f"Password: {find_credential.password}")
 
+        elif short_code == 'E':
+            print("\n")
+            print("Thank you for using password generator")
+            break
 
-
-
-
-
+        else:
+            print("\n")
+            print("Invalid input. Please try again")
+            print("\n")
 
     
-        
-    
-
-
-
-
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def copy_password(account):
-#     """
-#     function that copies password using pyperclip
-#     """
-#     return Credentials.copy_password(account)
-
-# def generate_password(account):
-#     """
-#     function that generates random password
-#     """
-#     _password = Credentials.generate_password()
-#     return _password
-
-
-
-# def main():
-#     print("Hi, Welcome to Password Manager...\n\n Please enter the following to proceed.\n\n CN -- Create a new account \n LI --Log in \n")
-#     short_code = input().lower()
-
-#     if short_code == "cn":
-#         print("Sign Up")
-#         print("-"*10)
-
-#         print("Enter Username: ")
-#         username = input()
-#         print("Enter email: ")
-#         email = input()
-#         print("Enter Password: ")     
-#         password = input()
-        
-#         save_user(create_new_user(username,email, password))
-#         print(f"Hi {username}, Welcome to Password Manager. Your email is {email} and Password is {password}")
-
-
-#     elif short_code == "li":
-#         print("Enter your password and log in")
-#         username = input("Username: ")
-#         password = input("Password: ")
-#         login = login_user(username,password)
-#         if login_user == login:
-#             print(f"Hello{username}. Welcome To Password Manager")
-#             print('\n')
-#         while True:
-#             print('Use the short codes: \n CC - Create new credentials \n DC- Display Credentials \n FC - Find credential \n GP - Generate a random password \n D- Delete credential \n EX - exit the application \n')
-#             short_code == input().lower().strip()
-#             if short_code == "cc":
-#                 print("Create a new contact")
-#                 account = ("Account Name: ")
-                
-#                 account = input().lower()
-#                 print("Username: ")
-#                 username = input()
-                
-
-
-
-
-    
-        # print("Please Log in")
-        # username = input("username: ")
-        # password = input("password: ")
-
-
-   
-
-
-#     else:
-#         print("Enter a valid choice")
-
-# if __name__ == '__main__':
-
-#     main()
-
-
-# def password_locker():
-#     print("Hi! Welcome to Password Manager..\n Please enter the following to proceed.\n CN -- Create a new account \n LI ---Log in \n")
-
-#     short_code = input("").lower().strip()
-#     if short_code == "cn":
-#         print("Sign Up")
-#         username = input("Username: ")
-#         while True:
-#             print("TP - To type your own password: \n GP - To generate random password")
-#             password_chosen = input().lower().strip()
-#             if password_chosen == "tp":
-#                 password = input("Enter Password\n")
-#                 break
-#             elif password_chosen == "gp":
-#                 password = generate_password()
-#                 break
-#             else:
-#                 print("Invalid choice please try again")
-#         save_user(create_new_user(username, password))
-#         print(f"Hello{username}, Your account has successfully been created. Your password is: {password}")
-    
-#     elif short_code == "li":
-#         print("Enter your password and log in")
-#         username = input("Username: ")
-#         password = input("Password: ")
-#         login = login_user(username,password)
-#         if login_user == login:
-#             print(f"Hello{username}. Welcome To Password Manager")
-#             print('\n')
-
-#     while True:
-#         print('Use the short codes: \n CC - Create new credentials \n DC- Display Credentials \n FC - Find credential \n GP - Generate a random password \n D- Delete credential \n EX - exit the application \n')
-#         short_code == input().lower().strip()
-#         if short_code == "cc":
-#             print("Create New Credential")
-#             print('Account name: ')
-#             account = input().lower()
-#             print("Username: ")
-#             username = input()
-#             while True:
-#                 print("TP - Type password or \n GP - To generate random password")
-#                 password_chosen == input().lower().strip()
-#                 if password_chosen == "tp":
-#                     password = input("Enter your password: \n")
-#                     break
-#                 elif password_chosen == 'gp':
-#                     password = generate_password()
-#                     break
-#                 else:
-#                     print("Invalid password choice please try again")
-
-#             save_credentials(create_new_user(account, username, password))
-#             print(f"Account Credential for: {account} - Username: {username} - Password:{password} created successfully")
-        
-#         elif short_code == "dc":
-#             if display_account_info():
-#                 print("Your list of accounts")
-#                 for account in display_account_info():
-#                     print(f"Account: {account.account} \n Username: {username}\n Password: {password}")
-#             else:
-#                 print("You don't have saved credentials yet")
-
-#         elif short_code == "fc":
-#             print("Enter the account name you want to search for")
-#             search_name = input().lower()
-#             if find_credential(search_name):
-#                 search_credential = find_credential(search_name)
-#                 print(f"Account Name: {search_credential.account}")
-#                 print(f"Username: {search_credential.account} Password: {search_credential.password}")
-            
-#             else:
-#                 print("The credentials doesn't exist")
-
-#         elif short_code == "d":
-#             print("Enter account name credential to be deleted")
-#             search_name = input().lower()
-#             if find_credential(search_name):
-#                 search_credential = find_credential(search_name)
-#                 search_credential.delete_credential()
-#                 print(f"Your stored credential for: {search_credential.account} successfully deleted")
-
-#             else:
-#                 print('Credentials to be deleted does not exist.')
-
-#         elif short_code == "gp":
-#             password = generate_password()
-#             print(f"{password} has been successfully generated.")
-
-#         elif short_code == 'ex':
-#             print("Thanks for using password manager. Successfully logged out")
-#             break
-#         else:
-#             print('Wrong entry. Ensure you match the suggested entries')
-#     else:
-#         print('Please enter a valid input to continue')
-        
-
-# if __name__ == "__main__":
-#     password_locker()
-
-
- # print("Hi, Welcome to Password Manager...\n\n Please enter the following to proceed.\n\n CN -- Create a new account \n LI --Log in \n")
-    # short_code == input("").lower().strip()
-    # if short_code == "cn":
-    #     print("Sign Up")
-    #     username = input("Username: ")
-    #     password = input("Password: ")
-    #     while True:
-
-    #         save_user(create_new_user(username, password))
-    #         print(f"Hello{username}! Your account has been successfully created")
-
-
-                
